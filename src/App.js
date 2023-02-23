8280ca8cfc3bd70da720c7224edf8b13a853f6e5
leungt30 <leungt30@mcmaster.ca>
2023-02-23 14:42:10 -0500

    working on contact

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
+import { Contact } from "./components/Contact2";
 import { Footer } from "./components/Footer";
 
 function App() {
--- src/components/Contact2.js
+++ src/components/Contact2.js
@@ -5,6 +5,10 @@ import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Contact = () => {
+  const email = 'timothyrleung@gmail.com'
+  const openEmail = () => {
+    window.location.href = `mailto:${email}`;
+  }
   const formInitialDetails = {
     firstName: '',
     lastName: '',
@@ -59,32 +63,11 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                      </Col>
-                    }
-                  </Row>
-                </form>
+                <h4>Email: </h4>
+                <button href="mailto:leungt30@gmail.com" style={{color:"white", border:"2px", backgroundColor: "purple", padding: "0.5rem 1rem", cursor: "pointer", ":hover": { backgroundColor: "green" }}}>
+                    {email}
+                </button>
+           
               </div>}
             </TrackVisibility>
           </Col>
